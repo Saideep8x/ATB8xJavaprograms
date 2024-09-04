22a16460cbb30acefbbc4a58e41c8a07c42b529a
Saideep Ramakuri <rsgdeep@gmail.com>
2024-09-04 15:19:09 +0530

    First code

--- src/sept/ex_04092024/Lab001.java
+++ src/sept/ex_04092024/Lab001.java
@@ -1,4 +1,22 @@
 package sept.ex_04092024;
 
+import java.util.Scanner;
+
 public class Lab001 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String name,batch;
+        System.out.println("Enter your name");
+        name=sc.next();
+        System.out.println("Enter your batch code");
+        batch=sc.next();
+        if(batch.equals("8X")){
+            System.out.println("Welcome to AutomationTesting by Pramod datta");
+        }
+        else{
+            System.out.println("Please enroll");
+        }
+        sc.close();
+
+    }
 }
