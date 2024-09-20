94b71d5833d818751f9b88ad1384c2923f9c95ed
Saideep Ramakuri <rsgdeep@gmail.com>
2024-09-20 19:30:00 +0530

    Write a program that classifies a triangle based on its side lengths.

---
+++ src/sept/ex_20092024/Lab003.java
@@ -0,0 +1,26 @@
+package sept.ex_20092024;
+
+import java.util.Scanner;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int side1,side2,side3;
+        System.out.println("Enter side1");
+        side1=sc.nextInt();
+        System.out.println("Enter side2");
+        side2=sc.nextInt();
+        System.out.println("Enter side3");
+        side3=sc.nextInt();
+        if(side1==side2 && side2==side3 && side1==side3){
+            System.out.println("Equilateral triangle");
+        }
+        else if (side1==side2 || side2==side3 || side1==side3){
+            System.out.println("Iscoceles triangle");
+        }
+        else {
+            System.out.println("Scalene triangle");
+        }
+
+    }
+}
