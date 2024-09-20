09fe78ebb61b3b13bb5c1c55d5fa13685f6a3234
Saideep Ramakuri <rsgdeep@gmail.com>
2024-09-20 19:54:31 +0530

    Write a program that prints numbers from 1 to 100. However, for multiples of 3, print "Fizz" instead of the number, and for multiples of 5, print "Buzz." For numbers that are multiples of both 3 and 5, print "FizzBuzz."

---
+++ src/sept/ex_20092024/Lab004.java
@@ -0,0 +1,29 @@
+package sept.ex_20092024;
+
+public class Lab004 {
+    //Write a program that prints numbers from 1 to 100. However, for multiples of 3,
+    // print "Fizz" instead of the number, and for multiples of 5, print "Buzz."
+    // For numbers that are multiples of both 3 and 5, print "FizzBuzz."
+
+    public static void main(String[] args) {
+        int i;
+        for(i=1;i<=100;i++){
+            System.out.print(i+" ");
+        }
+        for (i=1;i<=100;i++){
+            if(i%3==0){
+                System.out.print("\n"+"FIZZ");
+            }
+        }
+        for (i=1;i<=100;i++){
+            if(i%5==0){
+                System.out.print("\n"+"BUZZ");
+            }
+        }
+        for (i=1;i<=100;i++){
+            if(i%3==0 && i%5==0){
+                System.out.print("\n"+"FizzBuzz");
+            }
+        }
+    }
+}
