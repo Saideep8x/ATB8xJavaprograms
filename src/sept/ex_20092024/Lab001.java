73432999ff1f44ab6f6fef1751aa9f4b33de64ca
Saideep Ramakuri <rsgdeep@gmail.com>
2024-09-20 18:10:42 +0530

    Greatest of three numbers using scanner class

---
+++ src/sept/ex_20092024/Lab001.java
@@ -0,0 +1,18 @@
+package sept.ex_20092024;
+
+import java.util.Scanner;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a,b,c;
+        System.out.println("Enter the value of a");
+        a=sc.nextInt();
+        System.out.println("Enter the value of b");
+        b=sc.nextInt();
+        System.out.println("Enter the value of c");
+        c=sc.nextInt();
+        int max=a>b?a>c?a:c :(b>c)?b:c;
+        System.out.println("The maximum of three numbers is:"+max);
+    }
+}
