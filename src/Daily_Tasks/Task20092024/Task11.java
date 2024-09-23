20c8094afc565199f83a5e641cffd40fa91a6091
Saideep Ramakuri <rsgdeep@gmail.com>
2024-09-23 13:57:52 +0530

    Greatest of three numbers

--- src/Daily_Tasks/Task20092024/Task11.java
+++ src/Daily_Tasks/Task20092024/Task11.java
@@ -1,7 +1,7 @@
 package Daily_Tasks.Task20092024;
 
 import java.util.Scanner;
-
+//Maximum of three numbers
 public class Task11 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
