e00ff9bee2dd166f43a90a38c515390c98404886
Saideep Ramakuri <rsgdeep@gmail.com>
2024-09-25 10:31:55 +0530

    Table of number n = 10, print table with For or while

---
+++ src/Daily_Tasks/Task25092024/Task19.java
@@ -0,0 +1,12 @@
+package Daily_Tasks.Task25092024;
+//Table of number n = 10, print table with For or while.
+public class Task19 {
+    public static void main(String[] args) {
+        int n=10;
+
+        // For loop to generate the table of 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }
+    }
+}
